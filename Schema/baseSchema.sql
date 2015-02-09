a17821cacac7a9b6b4d6cd2f2410a6126bc78387
johnclin <faerellg@gmail.com>
2015-02-09 14:22:34 -0800

    base SQL schema, specific to mysql not mysqli

---
+++ Schema/baseSchema.sql
@@ -0,0 +1,15 @@
+create table users(
+	userID int not null auto_increment,
+	loginname char(45),
+	password char(45),
+	displayname char(45),
+	active int,
+	PRIMARY KEY (userID)
+);
+
+create table chatlog(
+	idchatlog int not null auto_increment,
+	userID int,
+	message char(255),
+	PRIMARY KEY (idchatlog)
+);
