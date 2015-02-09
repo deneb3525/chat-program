87a5fd904660a454e3b810dd866e3a68e4bf5529
johnclin <faerellg@gmail.com>
2015-02-09 15:46:19 -0800

    renaming DBAccess to BaseDAO.  refactoring in connection function

--- checkcreation.php
+++ checkcreation.php
@@ -1,13 +1,8 @@
 <?php
-	define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', 'admin');
-    define('DB_DATABASE', 'chatroom');
-    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-    if ($db->connect_error) {
-        die('Connect Error (' . $db->connect_errno . ') '
-                . $db->connect_error);
-    }
+include 'BaseDAO.php';
+	
+$goober = new BaseDAO();
+$db = $goober->connect();
 
 $tbl_name="users"; // Table name
 
